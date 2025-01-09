a94ec07a16e12f1c85248abd43d2fc72a7970655
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-08 22:19:55 -0500

    syle buttons

--- using-sc/frontend/components/App.js
+++ using-sc/frontend/components/App.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${pr => pr.color};
+  border: none;
+  border: 1px solid gray;
+  margin: 0.25rem;
+`
 
 function App() {
   const [color, setColor] = useState();
@@ -7,9 +15,9 @@ function App() {
     <div>
       <h2>Using SC</h2>
       <p>This is a styled React App</p>
-      <button>Orange</button>
-      <button>Blue</button>
-      <button>Purple</button>
+      <Button color='orange' onClick={() => setColor('orange')}>Orange</Button>
+      <Button color='blue' onClick={() => setColor('blue')}>Blue</Button>
+      <Button color='purple' onClick={() => setColor('purple')}>Purple</Button>
     </div>
   )
 }
